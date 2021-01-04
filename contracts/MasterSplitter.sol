c4a06e7b399c6b889ea8c35512428f51e2266740
ermz <eramirez4369@gmail.com>
2021-01-03 20:45:16 -0500

    Got rid of unnecessary struct, storage and made a much better kill function

--- contracts/MasterSplitter.sol
+++ contracts/MasterSplitter.sol
@@ -3,10 +3,17 @@ pragma solidity >=0.6.0 < 0.7.5;
 contract MasterSplitter {
     
     address payable public owner;
+    bool public isPaused = false;
     
     //I'm using owner address to create a kill switch at the bottom
     constructor () {
         owner = msg.sender;
+    }
+    
+    //Does this work better, I'm using your example from a question you answered on stack exchange
+    modifier onlyIfRunning {
+        require(isPaused == false);
+        _;
     }
     
     //Modifier used for the kill switch, so only the owner has access
@@ -21,38 +28,31 @@ contract MasterSplitter {
     }
     
     //This modifier is to check for empty/invalid addresses, though it might be unnecessary
+    //How would I go about checking for empty addresses, or should I just write a test for that 
     modifier validAddresses(address payeeOne, address payeeTwo) {
         require(payeeOne == address(payeeOne) && payeeTwo == address(payeeTwo), "Atleast one address is invalid!");
         _;
     }
     
-    // I made this public to get rid of the showMeTheMoney function I had previously
-    // The comment you left "The contract logic is indifferent to the name and we are using user address as a proxy for an id" really helped organize my thoughts
-    mapping (address => uint) public people;
-    
     uint amountSplit;
     uint splitRemainder;
     
-    // The if statement takes care of the supposed remainder of msg.value/2. I'm just awarding it to the first account
-    // I was thinking of transfering the splitRemainder after the fact, but sending an additional tranfer request seemed like it would cost additional gas
-    // If I'm understanding correctly I could also use .call, but it wouldn't make sense if it's not for a complex transfer that might need more gas?
-    function split(address payable payeeOne, address payable payeeTwo) external someoneElse(payeeOne, payeeTwo) validAddresses(payeeOne, payeeTwo) payable {
+    // The if statement will return the remainder to the sender, if there's any
+    function split(address payable payeeOne, address payable payeeTwo) external someoneElse(payeeOne, payeeTwo) onlyIfRunning validAddresses(payeeOne, payeeTwo) payable {
         amountSplit = msg.value / 2;
         splitRemainder = msg.value % 2;
-        if (splitRemainder > 0) {
-            payeeOne.transfer(amountSplit + splitRemainder);
-        } else {
-            payeeOne.transfer(amountSplit);
-        }
+        payeeOne.transfer(amountSplit);
         payeeTwo.transfer(amountSplit);
-        
-        people[msg.sender] = address(msg.sender).balance;
-        people[payeeOne] = address(payeeOne).balance;
-        people[payeeTwo] = address(payeeTwo).balance;
+        if (splitRemainder != 0) {
+            msg.sender.transfer(splitRemainder);
+        }
     }
     
-    function killSwitch() public onlyOwner{
-        selfdestruct(owner);
+    //Removed selfdestruct function and will now use a boolean instead
+    //I kept the onlyOwner modifier to avoid other stopping the split function
+    function stopContract() public onlyOwner {
+        isPaused = true;
     }
+    
     
 }
