3e072089f7793982a27266c1bd091c7a5264d66b
ermz <eramirez4369@gmail.com>
2021-01-07 22:23:17 -0500

    Corrected minor indentation issues on Pausable.sol file

--- contracts/Pausable.sol
+++ contracts/Pausable.sol
@@ -3,27 +3,22 @@ pragma solidity 0.7.4;
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract Pausable is Ownable {
-
 	bool public isPaused;
 
 	constructor () {
 		isPaused;
 	}
 
-	//Does this work better, I'm using your example from a question you answered on stack exchange
-    modifier onlyIfRunning public {
-        require(!isPaused, "Contract Paused");
-        _;
-    }
+	modifier onlyIfRunning public {
+		require(!isPaused, "Contract Paused");
+		_;
+	}
 
-    // Having one function that toggles isPaused "on" and "off" is better than having two separate functions
-    function contractSwitch() public onlyOwner {
-        if(isPaused) {
-        	isPaused = false;
-        } else {
-        	isPaused = true;
-        }
-    }
-
-
+	function contractSwitch() public onlyOwner {
+		if (isPaused) {
+			isPaused = false;
+		} else {
+			isPaused = true;
+		}
+	}
 }
